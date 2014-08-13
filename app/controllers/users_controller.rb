5532269ddd474acbe40f5b1f8949dc89bb63442a
minakshi-webonise <minakshi.najardhane@weboniselab.com>
2014-08-13 20:52:34 +0530

    insert update delete functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   def show
     @user = User.find(params[:id])
   end
@@ -7,8 +9,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+     @users = User.paginate(page: params[:page])
+  end
+
   def edit
     @user = User.find(params[:id])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
   end
 
   def update
@@ -41,14 +53,18 @@ class UsersController < ApplicationController
 
     def signed_in_user
       unless signed_in?
-        flash[:notice] = "Please sign in."
-        redirect_to signin_url
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
       end
     end
 
-    def correct_user
+     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
 
