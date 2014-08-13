f8f94bbc0560e5c88f67680026825da7ceb63343
minakshi-webonise <minakshi.najardhane@weboniselab.com>
2014-08-13 19:24:18 +0530

    finish signin sign ouit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -23,5 +38,18 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    def signed_in_user
+      unless signed_in?
+        flash[:notice] = "Please sign in."
+        redirect_to signin_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
 
 end
